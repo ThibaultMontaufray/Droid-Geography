5d3b092c2353e0ae9679c93cfc75811b7a7d63cb
Thibault MONTAUFRAY <amostrack@gmail.com>
2017-03-16 22:54:53 +1300

    first push for geography module

---
+++ Project/Droid-Geography/View/Demo.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Droid_Geography
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.worldMapView1 = new Droid_Geography.WorldMapView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // worldMapView1
+            // 
+            this.worldMapView1.BackColor = System.Drawing.Color.DimGray;
+            this.worldMapView1.CurrentWorldMap = null;
+            this.worldMapView1.Location = new System.Drawing.Point(0, 33);
+            this.worldMapView1.Name = "worldMapView1";
+            this.worldMapView1.Size = new System.Drawing.Size(669, 442);
+            this.worldMapView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "focus on";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(73, 6);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(173, 21);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(252, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "and";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(283, 6);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(173, 21);
+            this.comboBox2.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(682, 488);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.worldMapView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private WorldMapView worldMapView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox2;
+    }
+}
+
---
+++ Project/Droid-Geography/View/Demo.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Droid_Geography
+{
+    public partial class Form1 : Form
+    {
+        #region Attribute
+        private WorldMap _worldMap;
+        #endregion
+
+        #region Properties
+        public WorldMap CurrentWorldMap
+        {
+            get { return _worldMap; }
+            set { _worldMap = value; }
+        }
+        #endregion
+
+        #region Constructor
+        public Form1()
+        {
+            InitializeComponent();
+            Init();
+        }
+        #endregion
+
+        #region Methods public
+        #endregion
+
+        #region Methods private
+        private void Init()
+        {
+            _worldMap = new WorldMap();
+
+            comboBox1.Items.Clear();
+            comboBox2.Items.Clear();
+            foreach (Country country in _worldMap.Countries)
+            {
+                comboBox1.Items.Add(country.Name);
+                comboBox2.Items.Add(country.Name);
+            }
+        }
+        #endregion
+
+        #region Event
+        #endregion
+    }
+}
